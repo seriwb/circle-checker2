6c9b74c21e3648fb1a45170f6d673798b6e42eb1
seri <seri.wb@gmail.com>
2022-06-05 10:47:40 +0900

    add: フィルタ処理の追加

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod bootstrap;
 mod twitter;
 
+use egg_mode::user::TwitterUser;
+use regex::Regex;
 use twitter::Twitter;
 
 #[tokio::main]
@@ -9,21 +11,79 @@ async fn main() {
 
     let config = bootstrap::init();
 
-    // TODO: Twitter認証
     let mut twitter = Twitter::new(config.consumer_key, config.consumer_secret);
-    twitter.authenticate().await;
+    let token = twitter.authenticate().await;
 
-    println!("{:?}", twitter);
-    // TODO: Twitterから情報取得
-    // list users
-    // TODO: 結果を画面出力
-    print_header("\t");
+    let users_lists = twitter.get_user_lists(&token).await;
+    // println!("{:?}", &users_lists);
+
+    for target in config.cc.target_list {
+        let circles: Vec<CircleInfo> = if target == "" {
+            // タイムラインのデータを取得
+            let users = twitter.get_friends_list(&token).await;
+            // フィルタリングしたデータをCircleInfoに格納
+            users
+                .iter()
+                .filter_map(|user| check_user_info(user).ok())
+                .collect::<Vec<CircleInfo>>()
+        } else {
+            // リストのIDを取得
+            let list_id = &users_lists.iter().find(|x| *x.name == target);
+            if list_id.is_none() {
+                println!("リスト\"{}\"は存在しません", &target);
+                continue;
+            }
+            // リストのデータを取得
+            let users = twitter
+                .get_user_list_members(list_id.unwrap().id, &token)
+                .await;
+            // フィルタリングしたデータをCircleInfoに格納
+            users
+                .iter()
+                .filter_map(|user| check_user_info(user).ok())
+                .collect::<Vec<CircleInfo>>()
+        };
+
+        // 結果を画面出力
+        let title = if target == "" {
+            "タイムライン"
+        } else {
+            &target
+        };
+        println!("{}", title);
+        ountput(&circles);
+    }
+}
+
+fn check_user_info(user: &TwitterUser) -> Result<CircleInfo, String> {
+    // TODO: フィルタ群でマッチング
+    let filters = vec![r"(C9\d|Ｃ９[０-９]|C1\d\d|Ｃ１[０-９][０-９])(.*)".to_string()];
+    for filter in filters {
+        let hoge = format!(r"{}", &filter);
+        let re = Regex::new(&hoge).unwrap();
+        let caps = re.captures(&user.name);
+        if caps.is_none() {
+            continue;
+        }
+        let cap_strs = caps.unwrap();
+        return Ok(CircleInfo {
+            twitter_id: user.screen_name.clone(),
+            twitter_name: user.name.clone(),
+            twitter_url: user.url.clone().unwrap(),
+            match_string: cap_strs[1].to_string(),
+            space_string: cap_strs[2].to_string(),
+            profile_image_url: user.profile_image_url.clone(),
+            pinned_tweet_url: "TODO".to_string(),
+            pinned_image_urls: vec!["TODO 2".to_string()],
+        });
+    }
+    return Err("non".to_string());
 }
 
 #[derive(Debug)]
 struct CircleInfo {
-    twitter_name: String,
     twitter_id: String,
+    twitter_name: String,
     twitter_url: String,
     match_string: String,
     space_string: String,
@@ -31,9 +91,28 @@ struct CircleInfo {
     pinned_tweet_url: String,
     pinned_image_urls: Vec<String>,
 }
-impl CircleInfo {}
+impl CircleInfo {
+    fn output(&self, separator: &str) {
+        let line = vec![
+            &self.twitter_id,
+            &self.twitter_name,
+            &self.twitter_url,
+            &self.match_string,
+            &self.space_string,
+            &self.profile_image_url,
+            &self.pinned_tweet_url,
+        ];
+        println!("{}", line.iter().map(|x| x.as_str()).collect::<Vec<&str>>().join(&separator));
+    }
+}
 
-fn ountput(lists: &Vec<CircleInfo>) {}
+fn ountput(lists: &Vec<CircleInfo>) {
+    let separator = "\t";
+    print_header(&separator);
+    for info in lists {
+        info.output(&separator);
+    }
+}
 
 fn print_header(separator: &str) {
     let headers = [
--- src/twitter.rs
+++ src/twitter.rs
@@ -3,7 +3,12 @@ use std::{
     io::{stdout, Read, Write},
 };
 
-use egg_mode::{auth::verify_tokens, KeyPair, Token};
+use egg_mode::{
+    auth::verify_tokens,
+    list::{list, members, List},
+    user::{friends_of, TwitterUser},
+    KeyPair, Token,
+};
 use log::debug;
 use serde::{Deserialize, Serialize};
 
@@ -41,8 +46,8 @@ impl Twitter {
                 access_token: data.access_token.to_string(),
                 access_token_secret: data.access_token_secret.to_string(),
                 account: User {
+                    id: data.account.id,
                     name: data.account.name,
-                    screen_name: data.account.screen_name,
                     url: data.account.url,
                 },
             };
@@ -51,26 +56,28 @@ impl Twitter {
     }
 
     // 利用ユーザの認証を行う
-    pub async fn authenticate(&mut self) {
+    pub async fn authenticate(&mut self) -> Token {
         debug!("Twitter Authenticate");
 
-        let account = self.verify_credentials().await;
+        let result = self.verify_credentials().await;
         // まだアカウント情報がないor認証に失敗する場合はPIN認証のプロセスを実施
-        if !account.existed() {
-            self.oauth_process(self.consummer_key.clone(), self.consummer_secret.clone())
+        if !result.1.existed() {
+            let token = self
+                .oauth_process(self.consummer_key.clone(), self.consummer_secret.clone())
                 .await;
+            return token;
         } else {
-            self.account = account;
+            self.account = result.1;
+            return result.0;
         }
-        // self.account = self.verify_credentials().await;
     }
 
     // Twitterのユーザ認証を行い、アクセストークンを設定する
-    async fn oauth_process(&mut self, consummer_key: String, consummer_secret: String) {
+    async fn oauth_process(&mut self, consummer_key: String, consummer_secret: String) -> Token {
         debug!("OAuth authentication start.");
         // Twitter APIでアクセストークンを取得
         let token = get_access_tokens(consummer_key, consummer_secret).await;
-        println!("{:?}", token.0);
+
         match token.0 {
             Token::Access {
                 access: ref access_token,
@@ -81,17 +88,18 @@ impl Twitter {
             }
             _ => unreachable!(),
         }
-        // TODO: screen_nameが値になっている
+
         let name = &token.2;
         self.account = User {
+            id: token.1,
             name: (&name).to_string(),
-            screen_name: token.1.to_string(),
             url: format!("https://twitter.com/{}", &name),
         };
         debug!("OAuth authentication success.");
 
         self.store_access_tokens();
         debug!("OAuth authentication end.");
+        token.0
     }
 
     // AccessTokenをファイルに保存（アプリのキーは消す）
@@ -105,7 +113,7 @@ impl Twitter {
     }
 
     // アクセス可能なユーザ情報を取得
-    async fn verify_credentials(&self) -> User {
+    async fn verify_credentials(&self) -> (Token, User) {
         // https://docs.rs/egg-mode/latest/egg_mode/auth/fn.verify_tokens.html
         let token = Token::Access {
             consumer: KeyPair::new(self.consummer_key.clone(), self.consummer_secret.clone()),
@@ -115,24 +123,66 @@ impl Twitter {
 
         if twitter_user.is_ok() {
             // 認証できたら取得したユーザ情報を設定して返却
-            debug!("set account");
+            debug!("verified!");
             let data = twitter_user.unwrap();
-            debug!("{:?}", data);
-            User {
-                name: data.name.to_string(),
-                screen_name: data.id.to_string(),
-                url: format!("https://twitter.com/{}", data.name.to_string()),
-            }
+            (
+                token,
+                User {
+                    id: data.id,
+                    name: data.name.to_string(),
+                    url: format!("https://twitter.com/{}", data.name.to_string()),
+                },
+            )
         } else {
-            User {
-                ..Default::default()
-            }
+            (
+                token,
+                User {
+                    ..Default::default()
+                },
+            )
         }
     }
 
-    // pub fn get_user_lists(userinfo.getScreenName())
-    // pub fn get_user_list_members(targetList.id, -1L)
-    // pub fn get_friends_list(userinfo.id, -1L) // フォローユーザのリスト
+    // フォローユーザのリスト
+    pub async fn get_friends_list(&self, token: &Token) -> Vec<TwitterUser> {
+        let mut users: Vec<TwitterUser> = Vec::new();
+        loop {
+            let mut cursors = friends_of(self.account.id, &token).with_page_size(200);
+            let resp = cursors.call().await.unwrap();
+            users = [users, resp.response.users].concat();
+
+            let next = resp.response.next_cursor;
+            if next == 0 {
+                break;
+            }
+            cursors.next_cursor = next;
+        }
+        users
+    }
+
+    // 自分がオーナのリストを優先して100件取得
+    pub async fn get_user_lists(&self, token: &Token) -> Vec<List> {
+        let result = list(self.account.id, true, &token).await.unwrap();
+        result.response
+    }
+
+    // 指定のリストに含まれるユーザ情報を取得
+    pub async fn get_user_list_members(&self, list_id: u64, token: &Token) -> Vec<TwitterUser> {
+        let mut users: Vec<TwitterUser> = Vec::new();
+        loop {
+            let mut cursors =
+                members(egg_mode::list::ListID::ID(list_id), &token).with_page_size(200);
+            let resp = cursors.call().await.unwrap();
+            users = [users, resp.response.users].concat();
+
+            let next = resp.response.next_cursor;
+            if next == 0 {
+                break;
+            }
+            cursors.next_cursor = next;
+        }
+        users
+    }
 }
 
 // アクセストークンを取得(OAuth1.0a PIN認証のプロセスを実施)
@@ -187,15 +237,15 @@ fn load_access_tokens() -> Result<Twitter, String> {
 
 #[derive(Debug, PartialEq, Deserialize, Serialize, Clone)]
 pub struct User {
+    id: u64,
     name: String,
-    screen_name: String, //id
     url: String,
 }
 impl Default for User {
     fn default() -> User {
         User {
+            id: 0,
             name: "".to_string(),
-            screen_name: "".to_string(),
             url: "".to_string(),
         }
     }
